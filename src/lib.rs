7111220b3df832344c7e6de5d17ebb13e6b54e53
yash91989201 <yashraj6370@gmail.com>
2022-11-06 12:18:45 +0530

    test: create search function by TDD

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -24,5 +24,30 @@ impl Config {
         let filepath = args[2].clone();
 
         Ok(Config { query, filepath })
+    }
+}
+
+pub fn search<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe,fast,productive
+Pick three.
+        ";
+        assert_eq!(vec!["safe,fast,productive"], search(query, content))
     }
 }
