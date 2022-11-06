2b33d730a69f5e17bee24d4bce5fff34a0a83012
yash91989201 <yashraj6370@gmail.com>
2022-11-06 12:38:31 +0530

    fix: print error to standard error

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -17,7 +17,7 @@ fn main() {
     println!("In file {}", config.filepath);
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
